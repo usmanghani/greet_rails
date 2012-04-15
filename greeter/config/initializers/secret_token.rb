f3426ef1b82513cd9ef9fbdc797c7ce8083089aa
Usman Ghani <usman.ghani@gmail.com>
2012-04-15 01:33:43 -0700

    first checkin

---
+++ greeter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greeter::Application.config.secret_token = '204f43c7d7bea8f5e1b92cf986129040f370230c53791c4d5f930f2beb46c9d04fff683ef0e4ef7bc71d05a091f5b38e2fd8ed0e617c81624a3fd008e1ac7864'
